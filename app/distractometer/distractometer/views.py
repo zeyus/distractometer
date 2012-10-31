c10e68ab779e2d231bc9199b91e3d813e85193a4
root <root@beauty.smsglobal.local>
2012-10-31 14:06:27 +1100

    got view reading model

--- app/distractometer/distractometer/views.py
+++ app/distractometer/distractometer/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from .models import Distraction as Distraction
+from .models import Person as Person
 from django.db.models import Sum
 import json 
 
 def index(request):
 
-    distractions = Distraction.objects.values('person').annotate(Sum('duration'))
-    #distractions_day = json.dumps(distractions)
-    distraction_day = [{
-        #'time': d.time,
-        'duration': d.get('duration__sum'),
-        'person': d.get('person'),
-    } for d in distractions]
+    distractions = list(Distraction.objects.values('person').annotate(duration=Sum('duration')))
+#    distraction_day = json.dumps(distractions)
+    distraction_day = []
+    for d in distractions:
+        distraction_day.append({
+            'duration': d['duration'],
+            'person': Person.objects.get(pk=d['person']).name,
+        })
+
 
     return render_to_response('index.html', RequestContext(request,{'week':'asdf','day':distraction_day}))
 
